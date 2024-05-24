8a9690c78a195995c503fe6189d1537813319d87
goofy foot <williamk31@gmail.com>
2024-05-23 18:19:43 -0600

    fixed typo in server.js

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const startApolloServer = async () => {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) =>{
-      res.sendFile(path.join(__direname, 'client/dist/index.html'));
+      res.sendFile(path.join(__dirname, 'client/dist/index.html'));
     });
   }
 
